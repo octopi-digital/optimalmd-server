55573010e35bc12a0ac56b90dbb1f19d7198b1c7
emon3455 <emon.mhk69@gmail.com>
2024-11-22 16:11:11 +0600

    rxvalet proxy backend done

--- controller/rxvaletController.js
+++ controller/rxvaletController.js
@@ -29,6 +29,180 @@ async function login(req, res) {
   }
 }
 
+async function enrollment(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/enrollment.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
+async function updateMember(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/update_member.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
+async function addDependent(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/add_dependent.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
+async function updateDependent(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/update_dependent.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
+async function memberActivateOrDeactivate(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/member_deactivate_or_reactivate.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
+async function memberChangePlan(req, res) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(req.body).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    const response = await axios.post(
+      "https://rxvaletapi.com/api/omdrx/member_change_plan.php",
+      formData,
+      {
+        headers: {
+          api_key: "AIA9FaqcAP7Kl1QmALkaBKG3-pKM2I5tbP6nMz8",
+        },
+      }
+    );
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error calling the external API:", error.message);
+    res.status(error.response?.status || 500).json({
+      message: "Error calling the external API",
+      error: error.message,
+    });
+  }
+}
+
 module.exports = {
   login,
+  enrollment,
+  addDependent,
+  updateMember,
+  updateDependent,
+  memberActivateOrDeactivate,
+  memberChangePlan
 };
--- router/rxvaletRoutes.js
+++ router/rxvaletRoutes.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controller/rxvaletController");
+const { login, enrollment, addDependent, updateMember, updateDependent, memberActivateOrDeactivate, memberChangePlan } = require("../controller/rxvaletController");
 
 
-// Route to handle the external API request
+// member login
 router.post("/login", login);
+
+// member enrollment
+router.post("/enrollment", enrollment);
+
+// member enrollment
+router.post("/update-member", updateMember);
+
+// add dependent
+router.post("/add-dependent", addDependent);
+
+// update dependent
+router.post("/update-dependent", updateDependent);
+
+// member activate or deactivate
+router.post("/member-activate-deactivate", memberActivateOrDeactivate);
+
+// member change plan
+router.post("/member-change-plan", memberChangePlan);
 
 module.exports = router;
