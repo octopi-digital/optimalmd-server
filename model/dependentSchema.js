d1600071e1da670d0e46cbdc1706202c0156143f
emon3455 <emon.mhk69@gmail.com>
2024-11-27 20:00:54 +0600

    dependent updated

--- model/dependentSchema.js
+++ model/dependentSchema.js
@@ -1,31 +1,30 @@
 const mongoose = require('mongoose');
 
 const dependentSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String},
-  sex: { type: String, enum: ["Male", "Female", "Other"] },
+  firstName: { type: String, default: "" },
+  lastName: { type: String, default: "" },
+  sex: { type: String, enum: ["Male", "Female", "Other"], default: "" },
   relation: { type: String, enum: ["Parents", "Children", "Spouse", "Other"], required: true },
-  email: { type: String },
-  phone: { type: String },
-  dob: { type: String },
-  image: { type: String },
+  email: { type: String, default: "" },
+  phone: { type: String, default: "" },
+  dob: { type: String, default: "" },
+  image: { type: String, default: "" },
 
-  // user
   primaryUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 
-  // shipping info:
-  shipingAddress1: { type: String },
-  shipingAddress2: { type: String },
-  shipingCity: { type: String },
-  shipingState: { type: String },
-  shipingZip: { type: String },
+  // Shipping info:
+  shipingAddress1: { type: String, default: "" },
+  shipingAddress2: { type: String, default: "" },
+  shipingCity: { type: String, default: "" },
+  shipingState: { type: String, default: "" },
+  shipingZip: { type: String, default: "" },
 
-  // shipping info:
-  secondaryAddress1: { type: String },
-  secondaryAddress2: { type: String },
-  secondaryCity: { type: String },
-  secondaryState: { type: String },
-  secondaryZip: { type: String },
+  // Secondary address:
+  secondaryAddress1: { type: String, default: "" },
+  secondaryAddress2: { type: String, default: "" },
+  secondaryCity: { type: String, default: "" },
+  secondaryState: { type: String, default: "" },
+  secondaryZip: { type: String, default: "" },
 });
 
 module.exports = mongoose.model('Dependent', dependentSchema);
--- model/userSchema.js
+++ model/userSchema.js
@@ -6,36 +6,39 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   dob: { type: String, required: true },
-
-  image: { type: String },
+  image: { type: String, default: "" },
   password: { type: String },
-  resetPasswordToken: { type: String },
+  resetPasswordToken: { type: String, default: "" },
   resetPasswordExpires: { type: Date },
-
-  sPhone: { type: String },
-  sex: { type: String, enum: ["Male", "Female", "Other"] },
+  sPhone: { type: String, default: "" },
+  sex: { type: String, default: "" },
 
   // shipping info:
-  shipingAddress1: { type: String },
-  shipingAddress2: { type: String },
-  shipingCity: { type: String },
-  shipingState: { type: String },
-  shipingZip: { type: String },
+  shipingAddress1: { type: String, default: "" },
+  shipingAddress2: { type: String, default: "" },
+  shipingCity: { type: String, default: "" },
+  shipingState: { type: String, default: "" },
+  shipingZip: { type: String, default: "" },
 
   // secondary address:
-  secondaryAddress1: { type: String },
-  secondaryAddress2: { type: String },
-  secondaryCity: { type: String },
-  secondaryState: { type: String },
-  secondaryZip: { type: String },
+  secondaryAddress1: { type: String, default: "" },
+  secondaryAddress2: { type: String, default: "" },
+  secondaryCity: { type: String, default: "" },
+  secondaryState: { type: String, default: "" },
+  secondaryZip: { type: String, default: "" },
 
-  dependents: [{ type: mongoose.Schema.Types.ObjectId, ref: "Dependent" }],
-  paymentHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: "Payment" }],
-
-  status: {
-    type: String,
-    enum: ["Trial", "Canceled"],
+  dependents: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Dependent",
+    default: [],
   },
+  paymentHistory: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Payment",
+    default: [],
+  },
+
+  status: { type: String, enum: ["Active", "Canceled"], default: "Active" },
 
   // plans
   plan: {
@@ -43,34 +46,29 @@ const userSchema = new mongoose.Schema({
     enum: ["Trial", "Plus", "Access", "Premiere"],
     required: true,
   },
-  planPrice: { type: Number },
+  planPrice: { type: Number, default: 0 },
   planStartDate: { type: String },
   planEndDate: { type: String },
 
   // Payment details
-  paymentOption: { type: String },
-  // for bank
-  bankName: { type: String },
-  accountName: { type: String },
-  accountNumber: { type: String },
-  routingNumber: { type: String },
-  // for card
-  cardNumber: { type: String },
-  expiration: { type: String },
-  cvc: { type: String },
+  paymentOption: { type: String, default: "" },
+  bankName: { type: String, default: "" },
+  accountName: { type: String, default: "" },
+  accountNumber: { type: String, default: "" },
+  routingNumber: { type: String, default: "" },
+  cardNumber: { type: String, default: "" },
+  expiration: { type: String, default: "" },
+  cvc: { type: String, default: "" },
 
   // coupon
-  couponCode: { type: String },
+  couponCode: { type: String, default: "" },
 
   // rx-valet
-  PrimaryMemberGUID: { type: String },
+  PrimaryMemberGUID: { type: String, default: "" },
 
   // getlyric user id
-  lyricsUserId: {type:String},
-  ssoAccessToken: {type:String}
-
-  // 
-
+  lyricsUserId: { type: String, default: "" },
+  ssoAccessToken: { type: String, default: "" },
 });
 
 module.exports = mongoose.model("User", userSchema);
