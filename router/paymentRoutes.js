c0ecb83e0e152cc0f17e756bffc11719182a1e57
emon3455 <emon.mhk69@gmail.com>
2024-12-02 16:48:08 +0600

    payment done

--- router/paymentRoutes.js
+++ router/paymentRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { processPayment } = require("../controller/paymentController");
+const { processPayment, getAllPayment, getSinglePayment } = require("../controller/paymentController");
 
 router.post("/process", processPayment);
+
+router.get("/getAllPayment", getAllPayment);
+
+router.get("/getSinglePayment/:id", getSinglePayment);
 
 module.exports = router;
