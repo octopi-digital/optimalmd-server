4c5e0f6e5b4de56d334c9a571d318aebbe3adbd9
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 17:45:03 +0600

    add authorization to paymentRoute

--- router/paymentRoutes.js
+++ router/paymentRoutes.js
@@ -9,8 +9,13 @@ const auth = require("../middlewares/auth.middleware");
 
 router.post("/process", auth, processPayment);
 
-router.get("/getAllPayment", auth, getAllPayment);
+router.get("/getAllPayment", auth, authorize(["Admin"]), getAllPayment);
 
-router.get("/getSinglePayment/:id", auth, getSinglePayment);
+router.get(
+  "/getSinglePayment/:id",
+  auth,
+  authorize(["Admin"]),
+  getSinglePayment
+);
 
 module.exports = router;
