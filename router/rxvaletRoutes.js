7467f0d3e2f28c96c12fbf528dee19afd68ec3cf
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 17:50:34 +0600

    add authorization

--- router/rxvaletRoutes.js
+++ router/rxvaletRoutes.js
@@ -10,24 +10,35 @@ const {
   memberChangePlan,
 } = require("../controller/rxvaletController");
 const auth = require("../middlewares/auth.middleware");
+const authorize = require("../middlewares/authorize.middleware");
 
 // member login
-router.post("/login", login);
+router.post("/login", auth, authorize(["Admin", "User"]), login);
 
 // member enrollment
-router.post("/enrollment", auth, enrollment);
+router.post("/enrollment", auth, authorize(["Admin", "User"]), enrollment);
 
 // member enrollment
-router.post("/update-member", auth, updateMember);
+router.post("/update-member", auth, authorize(["Admin", "User"]), updateMember);
 
 // add dependent
-router.post("/add-dependent", auth, addDependent);
+router.post("/add-dependent", auth, authorize(["Admin", "User"]), addDependent);
 
 // update dependent
-router.post("/update-dependent", auth, updateDependent);
+router.post(
+  "/update-dependent",
+  auth,
+  authorize(["Admin", "User"]),
+  updateDependent
+);
 
 // member activate or deactivate
-router.post("/member-activate-deactivate", auth, memberActivateOrDeactivate);
+router.post(
+  "/member-activate-deactivate",
+  auth,
+  authorize(["Admin", "User"]),
+  memberActivateOrDeactivate
+);
 
 // member change plan
 router.post("/member-change-plan", auth, memberChangePlan);
