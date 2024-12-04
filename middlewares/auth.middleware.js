9a3e7d51f43eccd3155036e3433adc09790c44b1
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 17:28:31 +0600

    fix import

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/auth.model");
+const User = require("../model/userSchema");
 
 const auth = async (req, res, next) => {
   try {
