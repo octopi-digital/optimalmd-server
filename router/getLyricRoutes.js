011280136900d941605a1a39b7d5fad62c618262
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 14:52:45 +0600

    protect get lyric route

--- router/getLyricRoutes.js
+++ router/getLyricRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const { login, terminateUser } = require("../controller/getLyricController");
+const auth = require("../middlewares/auth.middleware");
 const router = express.Router();
-
 
 // member login
 router.post("/login", login);
-router.post("/terminate-user", terminateUser);
-
-
+router.post("/terminate-user", auth, terminateUser);
 
 module.exports = router;
