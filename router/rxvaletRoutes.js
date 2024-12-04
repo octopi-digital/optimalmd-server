4743a47567541f1a26a271724f1f3260d6931d4c
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 14:56:19 +0600

    protect rx valet routes

--- router/rxvaletRoutes.js
+++ router/rxvaletRoutes.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const { login, enrollment, addDependent, updateMember, updateDependent, memberActivateOrDeactivate, memberChangePlan } = require("../controller/rxvaletController");
-
+const {
+  login,
+  enrollment,
+  addDependent,
+  updateMember,
+  updateDependent,
+  memberActivateOrDeactivate,
+  memberChangePlan,
+} = require("../controller/rxvaletController");
+const auth = require("../middlewares/auth.middleware");
 
 // member login
 router.post("/login", login);
 
 // member enrollment
-router.post("/enrollment", enrollment);
+router.post("/enrollment", auth, enrollment);
 
 // member enrollment
-router.post("/update-member", updateMember);
+router.post("/update-member", auth, updateMember);
 
 // add dependent
-router.post("/add-dependent", addDependent);
+router.post("/add-dependent", auth, addDependent);
 
 // update dependent
-router.post("/update-dependent", updateDependent);
+router.post("/update-dependent", auth, updateDependent);
 
 // member activate or deactivate
-router.post("/member-activate-deactivate", memberActivateOrDeactivate);
+router.post("/member-activate-deactivate", auth, memberActivateOrDeactivate);
 
 // member change plan
-router.post("/member-change-plan", memberChangePlan);
+router.post("/member-change-plan", auth, memberChangePlan);
 
 module.exports = router;
