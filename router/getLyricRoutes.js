f3625ecc48da0a914b2d4858e2a034e602fd252a
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-12-04 17:40:13 +0600

    add authorization to get lyric

--- router/getLyricRoutes.js
+++ router/getLyricRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { login, terminateUser } = require("../controller/getLyricController");
 const auth = require("../middlewares/auth.middleware");
+const authorize = require("../middlewares/authorize.middleware");
 const router = express.Router();
 
 // member login
 router.post("/login", login);
-router.post("/terminate-user", auth, terminateUser);
+router.post(
+  "/terminate-user",
+  auth,
+  authorize(["User", "Admin"]),
+  terminateUser
+);
 
 module.exports = router;
